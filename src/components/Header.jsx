946f12e20012c09cce4a5e0e7a9b3c09f437b488
ShigaJun(cTwT) <shigajunctwt@gmail.com>
2024-10-09 00:41:04 +0900

    Reactでのコンポーネント化終了

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 export default function Header() {
     return (
-      <header className="hero is-dark is-bold">
-        <div className="hero-body">
-          <div className="container">
-            <h1 className="title">Birdwatching</h1>
-            <img src="../public/images/dove.png" alt="a simple dove logo"></img>
-          </div>
-        </div>
-      </header>
+        <header className="hero is-dark is-bold">
+            <div className="hero-body">
+                <div className="container">
+                    <h1 className="title">Birdwatching</h1>
+                    <img src="images/dove.png" alt="a simple dove logo"></img>
+                </div>
+            </div>
+        </header>
     );
-  }
+}

00e3be5c99b7f1cb454705ad235b9e8687d0f066
ShigaJun(cTwT) <shigajunctwt@gmail.com>
2024-10-13 22:08:50 +0900

    完成！！

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
+import Nav from "./Nav";
+
 export default function Header() {
     return (
-        <header className="header">                    <h1 className="title">Birdwatching</h1>
+        <header className="header">
+            <h1 className="title">Birdwatching</h1>
             <img src="images/dove.png" alt="a simple dove logo"></img>
+            <Nav />
         </header>
     );
 }
